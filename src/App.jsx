ccdafd3ff199b09e86ef62ff2aed04878f03174a
Mubasshir Ahmed <lastmubasshir@gmail.com>
2024-12-05 12:23:44 +0600

    custom color added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <main>
-      <h1 className="text-red-500 font-general font-bold text-5xl">
+      <h1 className="text-blue-300 font-general font-bold text-5xl">
         welcome to zentry
       </h1>
     </main>
